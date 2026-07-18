915647ca1cdb475048936f18163acb7ceac55ef9
Edgar Vazquez/WYMX/Wiwynn <Edgar_Vazquez@wiwynn.com>
2026-07-18 11:19:30 -0600

    feat: configure admin panel and untrack sqlite database

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Category,Post,Comment
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title","create_by","display_categories","created_at"]
+    list_filter = ["create_by","created_at"]
+    search_fields = ["title","create_by__username","categories__name"]
+    readonly_fields = ["created_at","last_modified"]
+
+    def display_categories(self,obj):
+        return ", ".join([category.name for category in obj.categories.all()])
+    
+    display_categories.short_description = "Categories"
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.create_by = request.user
+        super().save_model(request, obj, form, change)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["author","post","created_at"]
+    list_filter = ["created_at","post"]
+    search_fields = ["author__username","post__title"]
+    readonly_fields = ["created_at"]
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.author = request.user
+        super().save_model(request, obj, form, change)
+    
--- apps/blog/models.py
+++ apps/blog/models.py
@@ -14,7 +14,7 @@ class Category(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField(Category, related_name="category_posts")
     create_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_posts')
@@ -25,7 +25,7 @@ class Post(models.Model):
 class Comment(models.Model):
     author = models.ForeignKey(User,on_delete=models.CASCADE, related_name="user_comments")
     body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_comments")
 
     def __str__(self):
